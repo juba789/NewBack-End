f5e7f111eb707a12b08216344ac798246f44c0ae
juba <jubasyfax87@gmail.com>
2023-06-14 09:19:57 +0200

    création de la route de supression

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-require('dotenv').config()
-const express =require("express")
-const app = express()
-const cors =require("cors")
 const bodyParser = require('body-parser')
+const {app,express} =require("./server")
 const path = require("path")
 const port =3000
 
@@ -11,40 +8,23 @@ require("./mongo")
 
 //Controllers
 const {CreateUser,LogUser}= require("./controllers/users")
-const {getSauces,createSauce}=require("./controllers/sauces")
+const {getSauces,createSauce,getSauceById}=require("./controllers/sauces")
 
 
-//Middleware
-app.use(cors())
-app.use(express.json())
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-//app.use(express.static(path.join(__dirname,'images')))
+//Middleware
+const {upload}=require("./middleware/multer")
 const {authenticateUser} =require("./middleware/auth")
-const multer=require("multer")
-
-const storage = multer.diskStorage({
-    destination: "images/",
-    filename: function (req, file, cb) {
-      cb(null,makeFileName(req,file))
-    }
-  })
-
-  function makeFileName(req,file){
-    const fileName=`${Date.now()}-${file.originalname}`.replace(/\s/g,"-")
-    file.fileName=fileName
-    return fileName
-  }
-
-  const upload =multer({storage:storage})
-
 
 //Routes
 app.post("/api/auth/signup",CreateUser) 
 app.post("/api/auth/login",LogUser)
 app.get("/api/sauces",authenticateUser,getSauces)
 app.post("/api/sauces",authenticateUser,upload.single("image"), createSauce)
+app.get("/api/sauces/:id",authenticateUser,getSauceById)
 app.get("/", (req,res)=>res.send("h"))
 
 //Listen
