493a84a92f94423f82f1e4a1419185b048699a3f
juba <jubasyfax87@gmail.com>
2023-06-16 11:37:14 +0200

    fonction de modification avec image et de suppression de celle ci localement

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const unlink=require("fs").promises.unlink
+const { unlink } = require("fs/promises")
 
 
 const productSchema =new mongoose.Schema({
@@ -34,24 +34,79 @@ function getSauceById(req,res){
 function deleteSauce(req,res){
 const{id}=req.params
 Product.findByIdAndDelete(id)
-//.then(deleteImage)
 .then(product=>sendClientResponse(product,res))
+.then((item)=>deleteImage(item))
+.then((res)=>console.log("FILE DELETED",res))
 .catch(err=>res.status(500).send({message:err}))
 }
 
-/*function deleteImage(product){
-const {imageUrl}=product
-const fileToDelete=imageUrl.split("/").at(-1)
- return unlink(`images/${fileToDelete}`).then(()=>product)
-}*/
 
-function modifySauce(req,res){
+
+/*function modifySauce(req,res){
     const {params:{id}}=req
-    const {body}=req
-    console.log("body et id",body,id)
-    Product.findByIdAndUpdate(id,body)
+
+    
+    const hasNewImage = req.file != null
+    const payload =makePayload(hasNewImage,req)
+    
+
+    Product.findByIdAndUpdate(id,payload)
     .then((dbResponse)=>sendClientResponse(dbResponse,res))
+    .then((product)=>deleteImage(product))
+    .then((res)=>console.log("FILE DELETED",res))
     .catch((err)=>console.error("MAUVAIS UPDATING",err))
+}*/
+function modifySauce(req, res) {
+    const { params: { id } } = req;
+    const hasNewImage = req.file != null;
+    const payload = makePayload(hasNewImage, req);
+  
+    // Vérifier si une nouvelle image a été fournie
+    if (hasNewImage) {
+      Product.findById(id)
+        .then((product) => {
+          if (product == null) {
+            console.log("Sauce not found");
+            return res.status(404).send({ message: "Sauce not found in database" });
+          }
+          // Supprimer l'image précédente
+          const imageToDelete = product.imageUrl.split("/").at(-1);
+          return unlink("images/" + imageToDelete);
+        })
+        .then(() => {
+          // Mettre à jour la sauce avec le nouveau payload
+          return Product.findByIdAndUpdate(id, payload);
+        })
+        .then((dbResponse) => sendClientResponse(dbResponse, res))
+        .catch((err) => {
+          console.error("Error updating sauce:", err);
+          return res.status(500).send({ message: "Error updating sauce" });
+        });
+    } else {
+      // Aucune nouvelle image, mettre simplement à jour la sauce
+      Product.findByIdAndUpdate(id, payload)
+        .then((dbResponse) => sendClientResponse(dbResponse, res))
+        .catch((err) => {
+          console.error("Error updating sauce:", err);
+          return res.status(500).send({ message: "Error updating sauce" });
+        });
+    }
+  }
+  
+
+function deleteImage(product){
+if(product==null) return
+const imageToDelete=product.imageUrl.split("/").at(-1)
+ return unlink("images/"+imageToDelete)
+}
+
+function makePayload(hasNewImage,req){
+    console.log("hasNewImage",hasNewImage)
+    if(!hasNewImage) return req.body
+    const payload = JSON.parse(req.body.sauce)
+    payload.imageUrl=makeImageUrl(req,req.file.fileName)
+    console.log("VOICI LE PAYLOAD:",payload)
+    return payload
 }
 
 function sendClientResponse(product,res){
@@ -61,11 +116,13 @@ function sendClientResponse(product,res){
           return   res.status(404).send({message:"object not found in database"})
         }   
             console.log("bon updating",product)
-            res.status(200).send({message:"succes updated"})
+           return Promise.resolve(res.status(200).send({message:"succes updated"})).then(()=>product)
         
     
 }
-
+function makeImageUrl(req, fileName) {
+    return req.protocol + "://" + req.get("host") + "/images/" + fileName
+  }
 
 function createSauce(req,res){
     const{body,file}=req
@@ -73,9 +130,7 @@ function createSauce(req,res){
     const sauce = JSON.parse(body.sauce)
     const { name, manufacturer, description, mainPepper, heat, userId } = sauce
     
-    function makeImageUrl(req, fileName) {
-        return req.protocol + "://" + req.get("host") + "/images/" + fileName
-      }
+    
     
 const product = new Product({
     userId: userId,
--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get("/api/sauces",authenticateUser,getSauces)
 app.post("/api/sauces",authenticateUser,upload.single("image"), createSauce)
 app.get("/api/sauces/:id",authenticateUser,getSauceById)
 app.delete("/api/sauces/:id",authenticateUser,deleteSauce)
-app.put("/api/sauces/:id",authenticateUser,modifySauce)
+app.put("/api/sauces/:id",authenticateUser,upload.single("image"),modifySauce)
 app.get("/", (req,res)=>res.send("h"))
 
 //Listen
